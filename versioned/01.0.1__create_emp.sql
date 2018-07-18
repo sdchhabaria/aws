059c8f92787372ad91d06a05a4b7c176fcec5e82
sdchhabaria <41008248+sdchhabaria@users.noreply.github.com>
2018-07-18 23:33:09 +0530

    Update 01.0.1__create_emp.sql

--- versioned/01.0.1__create_emp.sql
+++ versioned/01.0.1__create_emp.sql
@@ -1,4 +1,26 @@
 create table schema_4.emp
 (emp_id number,
 emp_name varchar2(100),
-dept_id number);
+dept_id number,);
+
+
+Insert into schema_4.emp
+values(1,'abc',10);
+
+Insert into schema_4.emp
+values(2,'abcd',10);
+
+commit;
+
+
+create table schema_5.dept
+(dept_id number,
+dept_name varchar2(100));
+
+Insert into schema_5.dept
+values(10,'ábc');
+
+Insert into schema_5.dept
+values(20,'bcd');
+
+commit;
