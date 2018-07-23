b24cf0b5fbf4e6dbc3b2a07d6de747ce35021c8d
sdchhabaria <41008248+sdchhabaria@users.noreply.github.com>
2018-07-23 19:13:34 +0530

    Update and rename 01.0.1__create_emp.sql to V1.1__create_emp.sql

--- versioned/V1.1__create_emp.sql
+++ versioned/V1.1__create_emp.sql
@@ -1,26 +1,26 @@
-create table schema_4.emp
+create table qa_schema_1.emp
 (emp_id number,
 emp_name varchar2(100),
 dept_id number);
 
 
-Insert into schema_4.emp
+Insert into qa_schema_1.emp
 values(1,'abc',10);
 
-Insert into schema_4.emp
+Insert into qa_schema_1.emp
 values(2,'abcd',10);
 
 commit;
 
 
-create table schema_5.dept
+create table qa_schema_2.dept
 (dept_id number,
 dept_name varchar2(100));
 
-Insert into schema_5.dept
+Insert into qa_schema_2.dept
 values(10,'ábc');
 
-Insert into schema_5.dept
+Insert into qa_schema_2.dept
 values(20,'bcd');
 
 commit;
