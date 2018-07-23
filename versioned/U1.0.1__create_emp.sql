462a96ada522327b813274e559bdc8f113b46a0b
sdchhabaria <41008248+sdchhabaria@users.noreply.github.com>
2018-07-23 19:14:19 +0530

    Update and rename U1.0.1__create_emp.sql to U1.1__create_emp.sql

--- versioned/U1.0.1__create_emp.sql
+++
@@ -1,7 +0,0 @@
-truncate table schema_4.emp;
-
-drop table schema_4.emp;
-
-Delete from schema_5.dept;
-
-drop table schema_5.dept;
---
+++ versioned/U1.1__create_emp.sql
@@ -0,0 +1,7 @@
+truncate table qa_schema_1.emp;
+
+drop table qa_schema_1.emp;
+
+Delete from qa_schema_2.dept;
+
+drop table qa_schema_2.dept;
